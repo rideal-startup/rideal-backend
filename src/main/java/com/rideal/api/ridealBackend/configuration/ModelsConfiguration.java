cfbf19328937dc3e52bcda8786ad07c79e1a1828
MrceL97 <9719marcel@gmail.com>
2019-11-06 20:01:52 +0100

    Added validation on every Class (NotNull)

---
+++ src/main/java/com/rideal/api/ridealBackend/configuration/ModelsConfiguration.java
@@ -0,0 +1,19 @@
+package com.rideal.api.ridealBackend.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.core.mapping.event.ValidatingMongoEventListener;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
+
+@Configuration
+public class ModelsConfiguration {
+    @Bean
+    public ValidatingMongoEventListener validatingMongoEventListener() {
+        return new ValidatingMongoEventListener(validator());
+    }
+
+    @Bean
+    public LocalValidatorFactoryBean validator() {
+        return new LocalValidatorFactoryBean();
+    }
+}
