6547d383d225756a7698c36ac6d1d95dce3b4cf9
Guillem96 <guillem.orellana@gmail.com>
2019-12-11 15:49:31 +0100

    Setting firebase

--- src/main/java/com/rideal/api/ridealBackend/streaming/WebSocketSink.java
+++ src/main/java/com/rideal/api/ridealBackend/streaming/WebSocketSink.java
@@ -1,5 +1,8 @@
 package com.rideal.api.ridealBackend.streaming;
 
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.cloud.firestore.Firestore;
+import com.google.cloud.firestore.FirestoreOptions;
 import org.apache.flink.configuration.Configuration;
 import org.apache.flink.streaming.api.functions.sink.RichSinkFunction;
 import org.springframework.messaging.converter.MappingJackson2MessageConverter;
@@ -12,20 +15,38 @@ import org.springframework.web.socket.sockjs.client.SockJsClient;
 import org.springframework.web.socket.sockjs.client.Transport;
 import org.springframework.web.socket.sockjs.client.WebSocketTransport;
 
+import java.io.IOException;
 import java.util.Collections;
 import java.util.List;
 
 
 public class WebSocketSink extends RichSinkFunction<Message> {
     private transient StompSession session;
+    private transient Firestore db;
 
     private class MyHandler extends StompSessionHandlerAdapter {
         public void afterConnected(StompSession stompSession, StompHeaders stompHeaders) {
         }
     }
 
+    private void initializeFirebase() throws IOException {
+        final var serviceAccount =
+                this.getClass()
+                        .getClassLoader()
+                        .getResourceAsStream("rideal-startup-firebase-adminsdk-bdupn-1708a3a5b2.json");
+
+        final var firestoreOptions = FirestoreOptions
+                .getDefaultInstance().toBuilder()
+                .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                .build();
+
+        this.db = firestoreOptions.getService();
+    }
+
     @Override
     public void open(Configuration parameters) throws Exception {
+        initializeFirebase();
+
         List<Transport> transports = Collections.singletonList(
                 new WebSocketTransport(new StandardWebSocketClient()));
         final var transport = new SockJsClient(transports);
@@ -42,5 +63,9 @@ public class WebSocketSink extends RichSinkFunction<Message> {
     @Override
     public void invoke(Message value, Context context) {
         session.send("/chat", value);
+        this.db
+                .collection("lines-location")
+                .document(value.getLineId())
+                .set(value);
     }
 }
